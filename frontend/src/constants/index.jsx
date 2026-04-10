560acf87966d5bfe78be494e4db1ac45fc6d7565
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2026-04-10 14:22:49 +0700

    feat update constant

--- frontend/src/constants/index.jsx
+++ frontend/src/constants/index.jsx
@@ -1,9 +1,9 @@
-export const VAULT_SHARES_ADDRESS = "0x4A679253410272dd5232B3Ff7cF5dbB88f295319";
-export const THB_MOCK_ADDRESS = "0x322813Fd9A801c5507c9de605d63CEA4f2CE6c44"; 
-export const FUND_VAULT_ADDRESS = "0xa85233C63b9Ee964Add6F2cffe00Fd84eb32338f";
+export const VAULT_SHARES_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+export const THB_MOCK_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
+export const FUND_VAULT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 export const VAULT_SHARES_ABI = [
-  "function getNav() view returns (uint256)",
+  "function nav() view returns (uint256)",
   "function totalSupply() view returns (uint256)",
   "function balanceOf(address) view returns (uint256)",
   "function requestRedeem(uint256 shares) external",
